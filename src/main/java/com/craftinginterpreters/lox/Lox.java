0897343c3dfa4567681bd8e67c62bbe7c497d90f
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-08-09 20:56:38 -0300

    initial refactoring and laying out the skeleton

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -1,8 +1,6 @@
-package org.example;
+package com.craftinginterpreters.lox;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Lox {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
