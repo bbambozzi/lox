3bd26e7686bda4fc8a17e7f59375158ff7c56ca5
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-08-10 15:47:26 -0300

    added keywords hashmap

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,12 +1,35 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
 public class Scanner {
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
@@ -106,6 +129,12 @@ public class Scanner {
     }
 
 
+    private void identifier() {
+        while (isAlphaNumeric(peek())) {
+            advance();
+        }
+        addToken(IDENTIFIER);
+    }
 
     private void number() {
         while (isDigit(peek())) {
