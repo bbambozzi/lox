162b646d152f66951ae148386e79c36ae0824197
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-08-10 14:45:29 -0300

    Improved scanner and started the skeleton for adding numbers

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,15 +1,19 @@
 package com.craftinginterpreters.lox;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
 public class Scanner {
+
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
-    private int start =0;
+    private int start = 0;
     private int current = 0;
     private int line = 1;
+
     Scanner(String source) {
         this.source = source;
     }
@@ -28,16 +32,36 @@ public class Scanner {
     private void scanToken() {
         char c = advance();
         switch (c) {
-            case '(': addToken(LEFT_PAREN); break;
-            case ')': addToken(RIGHT_PAREN); break;
-            case '{': addToken(LEFT_BRACE); break;
-            case '}': addToken(RIGHT_BRACE); break;
-            case ',': addToken(COMMA); break;
-            case '.': addToken(DOT); break;
-            case '-': addToken(MINUS); break;
-            case '+': addToken(PLUS); break;
-            case ';': addToken(SEMICOLON); break;
-            case '*': addToken(STAR); break;
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
@@ -66,11 +90,33 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"':
+                string();
+                break;
             default:
+                if (isDigit(c)) {
+                    number();
+                } else {
                     Lox.error(line, "Unexpected Character");
-                    break;
+                }
+                break;
         }
     }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+        // we're at the final '"'
+        if (isAtEnd()) {
+            Lox.error(line, "Unfinished String! Reached EOF");
+        }
+
+        String stringValue = source.substring(start + 1, current - 1);
+        addToken(STRING, stringValue);
+    }
+
 
     private boolean match(char expected) {
         if (isAtEnd()) return false;
@@ -78,16 +124,29 @@ public class Scanner {
         current++;
         return true;
     }
+
     private char peek() {
         if (!isAtEnd()) return '\0';
         return source.charAt(current);
     }
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        // we avoid Character.isDigit() because it has accepts some characters we don't want
+        return c >= '0' && c <= '9';
+    }
+
     private boolean isAtEnd() {
         return current >= source.length();
     }
+
     private char advance() {
         return source.charAt(current++);
     }
+
     private void addToken(TokenType type) {
         addToken(type, null);
     }
