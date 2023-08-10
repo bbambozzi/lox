45280bc5c55500236d329fe59dc312a11d21a652
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-08-10 11:48:39 -0300

    expanded scanner to recognize tokens and added token consumption and basic token recognition

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -7,7 +7,53 @@ import static com.craftinginterpreters.lox.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start =0;
+    private int current = 0;
+    private int line = 1;
     Scanner(String source) {
         this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            /* at the beginning of the next lexeme */
+            start = current;
+            scanToken();
+        }
+    }
+
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            default:
+                    Lox.error(line, "Unexpected Character");
+                    break;
+        }
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+    private char advance() {
+        return source.charAt(current++);
+    }
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
